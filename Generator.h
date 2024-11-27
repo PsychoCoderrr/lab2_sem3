9281ce361ca814286be4ff46f56ef9715312e705
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-11-28 00:03:43 +0300

    started work with generator

--- Generator.h
+++ Generator.h
@@ -1,12 +1,14 @@
 #pragma once
-
+#include <fstream>
+#include <ctime>
+#include <random>
 std::string FirstNameList[20] = {
     "Ivan", "Andrei", "Dmitri", "Alexei", "Nikolai",
     "Sergei", "Mikhail", "Pavel", "Vladislav", "Oleg",
     "Yaroslav", "Roman", "Artem", "Viktor", "Boris",
     "Maxim", "Konstantin", "Gennady", "Leonid", "Stanislav"};
 
-std::string LastNameList[20] = {
+std::string SecondNameList[20] = {
     "Ivanov", "Petrov", "Sidorov", "Fedorov", "Kuznetsov",
     "Popov", "Smirnov", "Orlov", "Makarov", "Lebedev",
     "Egorov", "Morozov", "Volkov", "Novikov", "Pavlov",
@@ -28,4 +30,34 @@ std::string TitleNameList[] = {
     "Den strashnogo suda", "Kogda goryat mosty", "Tayna glubiny", "Legendy severa"
 };
 
-
+void Generator(std::string& name, int count)
+{
+    std::ofstream outFile(name);
+    
+    std::mt19937 generator(static_cast<unsigned int>(std::time(nullptr)));
+    
+    std::string title, authorSecondName, authorFirstName, ISBN;
+    int pages, YearPublish;
+    
+    outFile << "ISBN,title,authorSecondName,authorFirstName,pages,YearPublish";
+    
+    std::uniform_int_distribution<int> dist1(1000000, 9999999);
+    std::uniform_int_distribution<int> dist2(50, 2000);
+    std::uniform_int_distribution<int> dist3(1800, 2024);
+    std::uniform_int_distribution<int> distFirstName(0, 20);
+    std::uniform_int_distribution<int> distSecondName(0, 20);
+    std::uniform_int_distribution<int> distTitle(0, 60);
+    
+    for (int i = 0; i < count; i++)
+    {
+        ISBN = dist1(generator);
+        title = TitleNameList[distTitle(generator)];
+        authorFirstName = FirstNameList[distFirstName(generator)];
+        authorSecondName = SecondNameList[distSecondName(generator)];
+        pages = dist2(generator);
+        YearPublish = dist3(generator);
+        
+        BookCard(ISBN, title, authorSecondName, authorFirstName, pages, YearPublish);
+    }
+    
+}
--- main.cpp
+++ main.cpp
@@ -15,30 +15,30 @@
 int main()
 {
     
-    ArraySequence<int> seq;
-    
-    int size = 10000000;
-    int start_sequence = -10000;
-    int end_sequence = 10000;
-    int a;
-    for (int i = 0; i < size; i++)
-    {
-        a = rand() % (end_sequence - start_sequence + 1) + start_sequence;
-        seq.Append(a);
-    }
-    
-    QuickSort<int, ArraySequenceIterator<int, false>> HeapSorter;
-    clock_t start = clock();
-    HeapSorter.sort(seq.begin(), seq.end(), DefaultComparator);
-    clock_t end = clock();
-    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    std::cout << std::endl;
-    
+//    ArraySequence<int> seq;
+//    
+//    int size = 10000000;
+//    int start_sequence = -10000;
+//    int end_sequence = 10000;
+//    int a;
+//    for (int i = 0; i < size; i++)
+//    {
+//        a = rand() % (end_sequence - start_sequence + 1) + start_sequence;
+//        seq.Append(a);
+//    }
+//    
+//    QuickSort<int, ArraySequenceIterator<int, false>> HeapSorter;
+//    clock_t start = clock();
+//    HeapSorter.sort(seq.begin(), seq.end(), DefaultComparator);
+//    clock_t end = clock();
+//    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+//    std::cout << std::endl;
+//    
 //    std::cout << "After sorting:\n";
 //    for (auto it:seq) {
 //        std::cout << it << std::endl;
 //    }
 //    
-    std::cout << seconds << "\n";
+//    std::cout << seconds << "\n";
     return 0;
 }
